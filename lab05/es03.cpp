e8b1c1a41d624c1e74930702e100ee5b1ed6e62e
FedeBrus <federico.brusadin@gmail.com>
2024-09-30 09:53:32 +0200

    follia

--- lab05/es03.cpp
+++ lab05/es03.cpp
@@ -4,30 +4,28 @@ using namespace std;
 
 int main() {
     char c;
-    cout << "Inserire una lettera dell'alfabeto: " << endl;
-    cin >> c;
 
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-        switch (c) {
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
-            case 'A':
-            case 'E':
-            case 'I':
-            case 'O':
-            case 'U':
-                cout << "La lettera e' una vocale" << endl;
-                break;
+    do {
+        cout << "Inserire una lettera dell'alfabeto: " << endl;
+        cin >> c;
 
-            default:
-                cout << "La lettera e' una consonante" << endl;
-                break;
+        if (c <= 'Z') {
+            c += 'a' - 'A'; //converto tutto in minuscolo per ridurre i case dello switch
         }
-    } else {
-        cout << "Input errato" << endl;
+    } while (!(c >= 'a' && c <= 'z')); //richiedo l'input se il carattere non si trova nel range a...z
+
+    switch (c) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            cout << "La lettera e' una vocale" << endl;
+            break;
+
+        default:
+            cout << "La lettera e' una consonante" << endl;
+            break;
     }
 
     return 0;
